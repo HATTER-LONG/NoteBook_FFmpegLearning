bf02f2da55ecb61e22b311319cd06e5c03c3b658
HATTER-LONG <caolei6767@gmail.com>
2021-02-17 23:26:25 +0800

    编码器设置

--- LibFFmpegUsingExample/Src/01_FFmpegUsingExample/04_FFmpegReadNV12ToYum420BaseMac.c
+++ LibFFmpegUsingExample/Src/01_FFmpegUsingExample/04_FFmpegReadNV12ToYum420BaseMac.c
@@ -1,5 +1,6 @@
 #include "04_FFmpegReadNV12ToYum420BaseMac.h"
 
+#include <stdlib.h>
 #include <string.h>
 
 static int rec_status = 0;
@@ -79,12 +80,75 @@ static void read_data_and_encode(AVFormatContext* fmt_ctx, FILE* outfile)
     }
 }
 
+/**
+ *   @brief xxx
+ *   @param[int] xxx
+ */
+static void open_encoder(int width, int height, AVCodecContext** enc_ctx)
+{
+    AVCodec* codec = NULL;
+    codec = avcodec_find_encoder_by_name("libx264");
+    if (!codec)
+    {
+        printf("Codec libx264 not found\n");
+        exit(1);
+    }
+
+    *enc_ctx = avcodec_alloc_context3(codec);
+
+    if (!*enc_ctx)
+    {
+        printf("Could not allocate vide codec context!\n");
+        exit(1);
+    }
+
+    // SPS/PPS
+    (*enc_ctx)->profile = FF_PROFILE_H264_HIGH_444;
+    (*enc_ctx)->level = 50;   // 标识 Level 为 5.0
+
+    // 设置分辨率
+    (*enc_ctx)->width = width;
+    (*enc_ctx)->height = height;
+
+    // GOP 设置
+    (*enc_ctx)->gop_size = 250;
+    (*enc_ctx)->keyint_min = 25;   //可选 最小插入I帧间隔，如果在 GOP 过程中如果图像由突变会自动插入 I 帧
+
+    // 设置 B 帧数量
+    (*enc_ctx)->max_b_frames = 3;   //可选
+    (*enc_ctx)->has_b_frames = 1;   //可选
+
+    // 设置参考帧数量
+    (*enc_ctx)->refs = 3;   //可选
+
+    //设置输入 YUV 编码格式 libx264 要求输入为 420P
+    (*enc_ctx)->pix_fmt = AV_PIX_FMT_YUV420P;
+
+    //设置码率
+    (*enc_ctx)->bit_rate = 3150000;   // 3150 Kbps
+
+    //设置帧率
+    (*enc_ctx)->time_base = (AVRational) { 1, 25 };   // 帧与帧之间的间隔是 timebase
+    (*enc_ctx)->framerate = (AVRational) { 25, 1 };   // 帧率，每秒 25 帧与 timebase 成倒数关系
+
+    int ret = avcodec_open2((*enc_ctx), codec, NULL);
+    if (ret != 0)
+    {
+        char errors[1024] = {
+            0,
+        };
+        av_strerror(ret, errors, 1024);
+        fprintf(stderr, "Failed to open audio device, [%d]%s\n", ret, errors);
+        exit(1);
+    }
+}
+
 // void rec_audio()
 void rec_video()
 {
     // context
     AVFormatContext* fmt_ctx = NULL;
-
+    AVCodecContext* enc_ctx = NULL;
     // set log level
     av_log_set_level(AV_LOG_DEBUG);
 
@@ -112,8 +176,9 @@ void rec_video()
         printf("Error, Failed to open device!\n");
         goto __ERROR;
     }
+    //宽高与输入信息匹配
+    open_encoder(1920, 1080, &enc_ctx);
 
-    // open_encoder(int width, int height, enc_ctx);
     // encode
     read_data_and_encode(fmt_ctx, outfile);
 
